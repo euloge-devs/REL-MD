e55e5fa0ea46e3e75c7ca4f6cbaac9db71600e2a
euloge-devs <fantohoueuloge@gmail.com>
2025-08-27 13:12:27 +0100

    Enregistrement des nouveaux fichiers et modifications

--- web.js
+++ web.js
@@ -3,12 +3,9 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Sert tout le dossier public
+// Sert le dossier public
 app.use(express.static(path.join(__dirname, 'public')));
 
-// ...tes routes API...
-
-// Route d'accueil (optionnelle si tu utilises express.static)
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
