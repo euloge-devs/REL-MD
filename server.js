85b92caef53de35de7a49e14baeb8b413864af95
euloge-devs <fantohoueuloge@gmail.com>
2025-08-28 13:42:10 +0100

    Enregistrement des nouveaux fichiers et modifications

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Servir les fichiers statiques (HTML, CSS, JS)
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "Public")));
 
 // Route principale -> index.html
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "Public", "index.html"));
 });
 
 app.listen(PORT, () => {
